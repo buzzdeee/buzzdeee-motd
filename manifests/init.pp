aad4a2d10701066358242e75dc509dafa9f4ad0f
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2014-12-30 10:13:38 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,50 @@
+class motd {
+  $motd = '/etc/motd'
+
+  concat { $motd:
+    owner => 'root',
+    group => '0',
+    mode  => '0644'
+  }
+
+  concat::fragment{ 'motd_role_header':
+    target  => $motd,
+    content => "\nThis host is managed via Puppet, and has the following role:\n\n",
+    order   => '01'
+  }
+  concat::fragment{ 'motd_profile_header':
+    target  => $motd,
+    content => "\nThe following profiles belong to that role:\n\n",
+    order   => '15'
+  }
+}
+
+define motd::register_role($content="") {
+  if $content == "" {
+    $hostrole = $name
+  } else {
+    $hostrole = $content
+  }
+
+  concat::fragment{ "motd_role_fragment_$name":
+    target  => '/etc/motd',
+    order   => '10',
+    content => "   -- $hostrole\n"
+  }
+}
+
+define motd::register_profiles($profiles="") {
+  concat::fragment{ "motd_profile_fragment_$name":
+    target  => '/etc/motd',
+    order   => '20',
+    content => template("motd/profile.erb")
+  }
+}
+
+define motd::register_accounts($accounts="") {
+  concat::fragment{ "motd_accounts_fragment_$name":
+    target  => '/etc/motd',
+    order   => '30',
+    content => template("motd/accounts.erb"),
+  }
+}
